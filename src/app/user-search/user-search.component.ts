e030d9ca7b2c9970d8fb459436a8cdeb6dc7a7fb
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-11-19 11:39:48 +0100

    implemented

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,26 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, resource, signal } from '@angular/core';
+import { API_URL } from './config';
+import { User } from './model';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-user-search',
+  imports: [MatProgressBarModule],
   template: `
     <fieldset>
       <legend>Users Search</legend>
-      <input type="search" placeholder="Search...">
+      <input (input)="query.set($any($event.target).value)" type="search" placeholder="Search...">
     </fieldset>
+    @if (users.isLoading()) {
+      <mat-progress-bar mode="query" />
+    }
+    @if (users.error()) {
+      <div class="error">{{users.error()}}</div>
+    }
     <section class="actions">
-      <button>Reload</button>
-      <button>Add User</button>
-      <button>Clear</button>
+      <button (click)="users.reload()">Reload</button>
+      <button (click)="addUser()">Add User</button>
+      <button (click)="users.set([])">Clear</button>
     </section>
     <ul>
-      <li class="no-data">Nothing to show</li>
+      @for (user of users.value(); track user.id) {
+        <li>{{ user.name }}</li>
+      } @empty {
+        <li class="no-data">Nothing to show</li>
+      }
     </ul>
   `
 })
 export class UserSearchComponent {
+  query = signal('');
+  users = resource<User[], { query: string }>({
+    request: () => ({ query: this.query() }),
+    loader: async ({request, abortSignal}) => {
+      const users = await fetch(`${API_URL}?name_like=^${request.query}`, {
+        signal: abortSignal
+      });
+      if (!users.ok) throw Error(`Could not fetch...`)
+      return await users.json();
+    }
+  });
   constructor() {}
   addUser() {
     const user = { id: 123, name: "Dmytro Mezhenskyi" };
-    // add user logic
+    this.users.update(
+      users => users ? [user, ...users] : [user]
+    )
   }
 }
